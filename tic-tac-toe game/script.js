8c2f9b918e2e29c85235e4c720a20ef76c766f01
Ian Kariuki <kariukiian816@gmail.com>
2024-11-08 08:41:15 +0300

    Added the gameplay functionality to determine wins or ties

--- tic-tac-toe game/script.js
+++ tic-tac-toe game/script.js
@@ -9,7 +9,44 @@ const App = {
 	},
 
 	state: {
-		currentPlayer: 1,
+		moves: [],
+	},
+
+	getGameStatus(moves) {
+		const p1Moves = moves
+			.filter((move) => move.playerId === 1)
+			.map((move) => +move.squareId);
+		const p2Moves = moves
+			.filter((move) => move.playerId === 2)
+			.map((move) => +move.squareId);
+
+		console.log(p1Moves);
+
+		const winningPatterns = [
+			[1, 2, 3],
+			[1, 5, 9],
+			[1, 4, 7],
+			[2, 5, 8],
+			[3, 5, 7],
+			[3, 6, 9],
+			[4, 5, 6],
+			[7, 8, 9],
+		];
+
+		let winner = null;
+
+		winningPatterns.forEach((pattern) => {
+			const p1Wins = pattern.every((v) => p1Moves.includes(v));
+			const p2Wins = pattern.every((v) => p2Moves.includes(v));
+
+			if (p1Wins) winner = 1;
+			if (p2Wins) winner = 2;
+		});
+
+		return {
+			status: moves.length === 9 || winner != null ? "complete" : "in-progress",
+			winner, // 1 | 2 | null
+		};
 	},
 
 	// menu functionality
@@ -33,11 +70,29 @@ const App = {
 		// square grid functionality.
 		App.$.squares.forEach((square) => {
 			square.addEventListener("click", (e) => {
-				if (square.hasChildNodes()) {
+				// check if there is already a play, if so return early.
+
+				const hasMove = (squareId) => {
+					const existingMove = App.state.moves.find(
+						(move) => move.squareId === squareId
+					);
+					return existingMove !== undefined;
+				};
+
+				if (hasMove(+square.id)) {
 					return;
 				}
 
-				const currentPlayer = App.state.currentPlayer;
+				// Determine which player icon to add to the square.
+
+				const lastMove = App.state.moves.at(-1); // finding the last move in the move array.
+
+				const getOppositePlayer = (playerId) => (playerId === 1 ? 2 : 1);
+				const currentPlayer =
+					App.state.moves.length === 0
+						? 1
+						: getOppositePlayer(lastMove.playerId);
+
 				const icon = document.createElement("i");
 
 				if (currentPlayer === 1) {
@@ -46,9 +101,23 @@ const App = {
 					icon.classList.add("fa-solid", "fa-o", "turquoise");
 				}
 
-				App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1;
+				App.state.moves.push({
+					squareId: +square.id,
+					playerId: currentPlayer,
+				});
 
 				square.replaceChildren(icon);
+
+				// Check if there is a winner or tie game
+				const game = App.getGameStatus(App.state.moves);
+
+				if (game.status === "complete") {
+					if (game.winner) {
+						alert(`player ${game.winner} wins`);
+					} else {
+						alert("tie!");
+					}
+				}
 			});
 		});
 	},
